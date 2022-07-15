5ef3f0ad688d69d660839edce98864b1151e056f
wcgzgj <zhangjia_good@126.com>
2022-07-16 01:10:45 +0800

    解决活锁

--- src/main/java/bank_transfor/AccountWithoutLiveLockTransfer.java
+++ src/main/java/bank_transfor/AccountWithoutLiveLockTransfer.java
@@ -1,27 +1,32 @@
 package bank_transfor;
 
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
 
 /**
  * @ClassName AccountWithLiveLockTransfer
- * @Description 可能出现活锁的银行转账示例
+ * @Description 解决活锁（在释放锁前，随机等待一段时间）
  * @Author faro_z
  * @Date 2022/7/15 18:45
  * @Version 1.0
  **/
-public class AccountWithLiveLockTransfer {
+public class AccountWithoutLiveLockTransfer {
+
     private Long id;
     private String userName;
     private Long amount;
     public final ReentrantLock lock = new ReentrantLock();
+    private final Random r = new Random();
 
-    public AccountWithLiveLockTransfer(Long id, String userName, Long amount) {
+    public AccountWithoutLiveLockTransfer(Long id, String userName, Long amount) {
         this.id = id;
         this.userName = userName;
         this.amount = amount;
     }
 
-    public boolean transfer(AccountWithLiveLockTransfer to,Long amount) {
+    public boolean transfer(AccountWithoutLiveLockTransfer to, Long amount) {
+
         boolean doLoop = true;
         while (doLoop) {
             // 非阻塞获取锁
@@ -29,7 +34,8 @@ public class AccountWithLiveLockTransfer {
             // 但是如果 A,B  和  B,A 的资源在同时被两个线程尝试获取，会发生"活锁"问题
             if (this.lock.tryLock()) {
                 try {
-                    if (to.lock.tryLock()) {
+                    // 让等待下一个锁的时间随机，避开活锁
+                    if (to.lock.tryLock(r.nextInt(100), TimeUnit.MILLISECONDS)) {
                         try {
                             if (this.amount<amount) {
                                 return false;
@@ -41,6 +47,8 @@ public class AccountWithLiveLockTransfer {
                             to.lock.unlock();
                         }
                     }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 } finally {
                     this.lock.unlock();
                 }
