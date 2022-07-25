835807c32258f279a2e2c1b2bdf2d8b4c5d6e349
wcgzgj <zhangjia_good@126.com>
2022-07-25 15:51:09 +0800

    CyclicBarrier 添加注释，解释为什么 CB 回调函数要使用大小为 1 的线程池

--- src/main/java/cdl_and_cb/CBExample.java
+++ src/main/java/cdl_and_cb/CBExample.java
@@ -34,6 +34,14 @@ public class CBExample {
      * 执行差 diff 和 save 的操作
      */
     private void check() {
+        // 下面这两个操存在竞态（虽然 vector.remove是线程阿安全的，但是两个操作都执行不一定是原子的）
+        // 比如两个线程 T1，T2，
+        // T1 执行 pOrders.remove(0)，获得 p1，线程切换;
+        // T2 执行 pOrders.remove(0)，获得 p2；执行 dOrders.remove(0)，获得 d1，线程切换；
+        // T1 执行 dOrders.remove(0)，获得 d2；
+        // 最后结果就是 T1 获得 p1,d2，T2 获得 p2,d1，不符合要求
+        // 所以 CyclicBarrier 的回调函数中，我们才设置 大小为1的线程池
+        // 目的就是让 check() 相对于主线程并行，但是 check() 之间是串行的，从而达到线程安全的效果
         PreOrder preOrder = preOrders.remove(0);
         DeliveryOrder deliveryOrder = deliveryOrders.remove(0);
         Diff diff = null;
