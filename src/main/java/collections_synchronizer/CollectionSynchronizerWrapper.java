a962c00e1669416db7a38d0cb6d9939feb1075a5
wcgzgj <zhangjia_good@126.com>
2022-07-25 16:46:41 +0800

    转换并发安全的集合

---
+++ src/main/java/collections_synchronizer/CollectionSynchronizerWrapper.java
@@ -0,0 +1,18 @@
+package collections_synchronizer;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * @ClassName CollectionSynchronizerWrapper
+ * @Description Collections 工具类可以将非并发安全的集合，转换为并发安全的集合
+ * @Author faro_z
+ * @Date 2022/7/25 16:16
+ * @Version 1.0
+ **/
+public class CollectionSynchronizerWrapper {
+    public static void main(String[] args) {
+        List<Integer> synchronizedList = Collections.synchronizedList(new ArrayList<Integer>());
+    }
+}
